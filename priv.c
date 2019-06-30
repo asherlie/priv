108d0a7e10477c4e55673b1c95ee636f687f98c0
asherlie <lieber.asher@gmail.com>
2019-06-30 14:10:34 -0400

    chars from decoy file are now printed as chars are read from stdin

--- priv.c
+++ priv.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <termios.h>
 
-#define DB 1
+#define DB 0
 
 int main(int a, char** b){
       if(a < 2)return 0;
@@ -16,13 +16,19 @@ int main(int a, char** b){
 
       FILE* out_fp, * decoy_fp;
 
+      /* TODO: use all args after first as input for decoy */
+      /* if only one is give, it repeats after running out of chars */
       decoy_fp = (a > 2) ? fopen(b[2], "r") : NULL;
 
       out_fp = fopen(b[1], "a");
 
-      char c;
+      char c, dc;
       while((c = fgetc(stdin)) != EOF){
             if(DB)fputc(c, stdout);
+            else if(decoy_fp){
+                  if((dc = fgetc(decoy_fp)) == EOF)fseek(decoy_fp, 0L, SEEK_SET);
+                  fputc(dc, stdout);
+            }
             fputc(c, out_fp);
             if(c == 'q')break;
       }
