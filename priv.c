5103889cd21ee6790e85385e58432b0754d234ef
asherlie <lieber.asher@gmail.com>
2019-07-01 15:33:21 -0400

    added clear_screen()

--- priv.c
+++ priv.c
@@ -1,7 +1,18 @@
+/* TODO: this should be a curses program */
 #include <stdio.h>
+#include <stdlib.h>
 #include <termios.h>
+#include <unistd.h>
+
+#include <sys/ioctl.h>
 
 #define DB 0
+
+void clear_screen(){
+      struct winsize w;
+      ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+      for(int i = 0; i < w.ws_row; ++i)puts("");
+}
 
 int main(int a, char** b){
       if(a < 2)return 0;
@@ -22,17 +33,35 @@ int main(int a, char** b){
 
       out_fp = fopen(b[1], "a");
 
-      char c, dc;
+      /* we keep track of this for when we need to switch back to decoy mode
+       * after printing written text
+       */
+      int n_decoy = 0, out_sz = 0, out_cap = 50;
+      char c, dc, * out_txt = calloc(out_cap, 1);
+
+
       while((c = fgetc(stdin)) != EOF){
+            /*printf("%i\n", c);*/
+
+            if(c == 'D'){
+                  clear_screen();
+            }
+
+            if(c == 3 || c == 4)break;
             if(DB)fputc(c, stdout);
             else if(decoy_fp){
-                  if((dc = fgetc(decoy_fp)) == EOF)fseek(decoy_fp, 0L, SEEK_SET);
+                  if((dc = fgetc(decoy_fp)) == EOF){
+                        ++n_decoy;
+                        fseek(decoy_fp, 0L, SEEK_SET);
+                  }
+                  if(dc == '\n')fputc('\r', stdout);
                   fputc(dc, stdout);
             }
+            /* backspace */
+            if(c == 127)fprintf(out_fp, "<DEL>");
             fputc(c, out_fp);
-            if(c == 'q')break;
       }
-      fputc(0, out_fp);
+      /*fputc(0, out_fp);*/
       fputc('\n', out_fp);
 
       /* exit raw mode */
